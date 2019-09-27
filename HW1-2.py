ce48d6860881c1fb675bd837dbb34046b85f656b
Govrie <55189059+Govrie@users.noreply.github.com>
2019-09-27 23:38:17 +0900

    Исправлено 27-09-2019, 23:40

--- HW1-2.py
+++ HW1-2.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3 -tt
 """File: hello.py """
+def digits_sum(n):
+    string_repr = str(abs(n))
+    digits_sum = 0
+    for digit in string_repr:
+        digits_sum += int(digit)
+    return digits_sum
 def is_beauty(n):
-    def get_digit_sum(n):
-        string_repr = str(abs(n))
-        digits_sum = 0
-        for digit in string_repr:
-            digits_sum +=int(digit)
-        return digits_sum
-    return n % get_digit_sum(n) == 0
+    return n % digits_sum(n) == 0
 #Run only if called as a script
 if __name__ == '__main__':
-    print(is_beauty(12))
+    n = int(input("Enter a number = "))
+    print(is_beauty(n))
